db0921413fe85bbf6e1ddc5d4d79fa0e614f84be
const <sqb@mail.ru>
2023-06-19 10:54:02 +0200

    Initial Commit

---
+++ drawing-app/drawing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_appApp.swift
+//  drawing-app
+//
+//  Created by const on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct drawing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
